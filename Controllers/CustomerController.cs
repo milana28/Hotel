ef69b03321568bc0a20442ce5373a9ad0c093452
Milana <milana.bijeljanin@gmail.com>
2020-07-21 12:08:23 +0200

    CustomerController

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Hotel.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,9 +21,49 @@ namespace Hotel.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status405MethodNotAllowed)]
-        public Models.Customer CheckIn(Models.Customer customer)
+        public ActionResult<Models.Customer> CreateCustomer(Models.Customer customer)
         {
-            return _customer.CheckIn(customer);
+            return _customer.CreateCustomer(customer);
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<Models.Customer>> GetCustomers()
+        {
+            return _customer.GetAll();
+        }
+        
+        [HttpGet("{customerId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Models.Customer> GetCustomerInfo(int customerId)
+        {
+            var customer = _customer.GetCustomerById(customerId);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Models.Customer> DeleteCustomer(int guestId)
+        {
+            var guest = _customer.GetCustomerById(guestId);
+            
+            if (guest == null)
+            {
+                return NotFound();
+            }
+            
+            return _customer.DeleteCustomer(guestId);
         }
       
     }
--- Domain/Customer.cs
+++ Domain/Customer.cs
@@ -1,24 +1,30 @@
+using System.Collections.Generic;
 using System.Data;
-using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
 using Hotel.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Hotel.Domain
 {
     public interface ICustomer
     {
-        Models.Customer CheckIn(Models.Customer guest);
+        Models.Customer CreateCustomer(Models.Customer guest);
+        Models.Customer DeleteCustomer(int guestId);
+        Models.Customer GetCustomerById(int id);
+        List<Models.Customer> GetAll();
 
     }
     
     public class Customer : ICustomer
     {
         private const string DatabaseConnectionString = "Server=localhost;Database=hotel;User Id=sa;Password=yourStrong(!)Password;";
+        private readonly List<Models.Customer> _customers = new List<Models.Customer>();
 
-        public Models.Customer CheckIn(Models.Customer guest)
+        public Models.Customer CreateCustomer(Models.Customer guest)
         {
-            var newGuest = new CustomerDAO()
+            var guestDao = new CustomerDAO()
             {
                 Name = guest.Name,
                 PhoneNo = guest.PhoneNo,
@@ -33,9 +39,39 @@ namespace Hotel.Domain
         
             using IDbConnection database = new SqlConnection(DatabaseConnectionString); 
             const string insertQuery = "INSERT INTO Hotel.Customer VALUES (@name, @phoneNo, @address, @roomNo)";
-            database.Execute(insertQuery, newGuest);
+            database.Execute(insertQuery, guestDao);
 
-            return TransformDaoToBusinessLogicCustomer(newGuest);
+            return TransformDaoToBusinessLogicCustomer(guestDao);
+        }
+
+        public Models.Customer DeleteCustomer(int guestId)
+        {
+            using IDbConnection database = new SqlConnection(DatabaseConnectionString);
+            const string sql = "DELETE FROM Hotel.Customer WHERE id = @customerId";
+            
+            database.Execute(sql, new {customerId = guestId});
+
+            return GetCustomerById(guestId);;
+        }
+        
+        public Models.Customer GetCustomerById(int id)
+        {
+            using IDbConnection database = new SqlConnection(DatabaseConnectionString);
+            const string sql = "SELECT * FROM Hotel.Customer WHERE id = @customerId";
+            
+            var customerDao = database.QuerySingle<CustomerDAO>(sql, new {customerId = id});
+
+            return TransformDaoToBusinessLogicCustomer(customerDao);
+        }
+
+        public List<Models.Customer> GetAll()
+        {
+            using IDbConnection database = new SqlConnection(DatabaseConnectionString);
+            var customerDaoList = database.Query<CustomerDAO>("SELECT * FROM Hotel.Customer").ToList();
+
+            customerDaoList.ForEach(e => _customers.Add(TransformDaoToBusinessLogicCustomer(e)));
+
+            return _customers;
         }
 
         private Models.Customer TransformDaoToBusinessLogicCustomer(CustomerDAO customerDao)
@@ -46,6 +82,7 @@ namespace Hotel.Domain
 
             return new Models.Customer()
             {
+                Id = customerDao.Id,
                 Name = customerDao.Name,
                 PhoneNo = customerDao.PhoneNo,
                 Address = customerDao.Address,
