5217c8f8b26acc8cb50158e7dbfe8123884aabc8
Milana <milana.bijeljanin@gmail.com>
2020-07-31 11:34:01 +0200

    Reformat code

--- Domain/Bill.cs
+++ Domain/Bill.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Hotel.Models;
 
 namespace Hotel.Domain
 {
--- Domain/Room.cs
+++ Domain/Room.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
-using Hotel.Models;
 
 namespace Hotel.Domain
 {
